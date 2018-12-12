6998f28871eef56898c05a4e9d23ee206b75a712
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-12-12 18:45:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,27 +3,43 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    s = self.new
+    s.id, s.name, s.grade = row
+    s
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
+    sql = <<-SQL
+              SELECT * From students
+    SQL
     # remember each row should be a new instance of the Student class
+    DB[:conn].execute(sql).map do |row|
+     self.new_from_db(row)
+   end
+
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
+    sql = <<-SQL
+              SELECT * From students where name = ? Limit 1
+    SQL
     # return a new instance of the Student class
+    DB[:conn].execute(sql, name).map do |row|
+     self.new_from_db(row)
+   end.first
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
